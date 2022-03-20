da07461b046abaa14f127f4cc93c9de38435fb2c
EmiNiemand <Szymon_PL@live.com>
2022-03-20 12:51:22 +0100

    Add basic project
    Simple Class and Tests

---
+++ TPW/LogicTests/HelloWorldTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TPW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPW.Tests
+{
+    [TestClass()]
+    public class HelloWorldTests
+    {
+        [TestMethod()]
+        public void GetGreetingTest()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            Assert.AreEqual(helloWorld.GetGreeting("Bartek"), "Hello Bartek");
+        }
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            Assert.AreEqual(helloWorld.Sum(2, 5), 7);
+        }
+    }
+}
---
+++ TPW/TPW/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿namespace TPW
+{
+    public class HelloWorld
+    {
+        public string GetGreeting(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public float Sum(float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
