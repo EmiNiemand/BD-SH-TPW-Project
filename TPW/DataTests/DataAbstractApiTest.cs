72892a3cc1fdd3351183aad5994334277b74debe
bortok1 <xbortokx@gmail.com>
2022-04-26 19:13:11 +0200

    Minor fix DataTests

--- TPW/DataTests/DataAbstractApiTest.cs
+++ TPW/DataTests/DataAbstractApiTest.cs
@@ -22,8 +22,8 @@ namespace DataTests
             Assert.AreEqual(ball1.GetPosition(), ball2.GetPosition());
             Assert.AreEqual(ball1.GetRadius(), ball2.GetRadius());
             IBall ball3 = data.CreateBall(new System.Numerics.Vector2(4, 8), 10);
-            Assert.AreEqual(ball1.GetPosition(), ball3.GetPosition());
-            Assert.AreEqual(ball1.GetRadius(), ball3.GetRadius());
+            Assert.AreNotEqual(ball1.GetPosition(), ball3.GetPosition());
+            Assert.AreNotEqual(ball1.GetRadius(), ball3.GetRadius());
         }
     }
 }
