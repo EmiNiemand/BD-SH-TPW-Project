8cb74ce135c6b89d056cfa51aab7327cb37e7cdf
bortok1 <xbortokx@gmail.com>
2022-05-30 00:54:52 +0200

    Collision fix

--- TPW/Logic/CollisionHandler.cs
+++ TPW/Logic/CollisionHandler.cs
@@ -30,8 +30,8 @@ namespace Logic
         {
             Vector2 ball1NextPos = ball1.position + (Vector2.One * ball1.ballD / 2) + ball1.direction;
             Vector2 ball2NextPos = ball2.position + (Vector2.One * ball2.ballD / 2) + ball2.direction;
-            float ballsDistance = (ball1NextPos.X - ball2NextPos.X) * (ball1NextPos.X - ball2NextPos.X) + (ball1NextPos.Y - ball2NextPos.Y) * (ball1NextPos.Y - ball2NextPos.Y);
-            float ballsRDistance = (ball1.ballD + ball2.ballD) * (ball1.ballD + ball2.ballD) / 4;
+            float ballsDistance = (float)(Math.Pow(ball1NextPos.X - ball2NextPos.X, 2) + Math.Pow(ball1NextPos.Y - ball2NextPos.Y, 2));
+            float ballsRDistance = (float)Math.Pow(ball1.ballD / 2 + ball2.ballD / 2, 2);
             return ballsDistance <= ballsRDistance;
         }
 
@@ -54,17 +54,18 @@ namespace Logic
             Vector2 ball1center = ball1.position + (Vector2.One * ball1.ballD / 2);
             Vector2 ball2center = ball2.position + (Vector2.One * ball2.ballD / 2);
 
-            double centerDistancePow1 = Math.Pow((ball1center.X - ball2center.X), 2) + Math.Pow((ball1center.Y - ball2center.Y), 2);
-            float massCalculation1 = (2 * ball2.mass / (ball1.mass + ball2.mass));
-            float dotProduct1 = Vector2.Dot((ball1.direction - ball2.direction), (ball1center - ball2center));
+            float centerDistancePow1 = (float)(Math.Pow(ball1center.X - ball2center.X, 2) + Math.Pow(ball1center.Y - ball2center.Y, 2));
+            float massCalculation1 = 2 * ball2.mass / (ball1.mass + ball2.mass);
             Vector2 Oi1 = ball1center - ball2center;
-            ball1.direction -= massCalculation1 * dotProduct1 / (float)centerDistancePow1 * Oi1;
+            float dotProduct1 = Vector2.Dot(ball1.direction - ball2.direction, Oi1);
 
-            double centerDistancePow2 = Math.Pow((ball2center.X - ball1center.X), 2) + Math.Pow((ball2center.Y - ball1center.Y), 2);
-            float massCalculation2 = (2 * ball1.mass / (ball1.mass + ball2.mass));
-            float dotProduct2 = Vector2.Dot((ball2.direction - ball1.direction), (ball2center - ball1center));
+            float centerDistancePow2 = (float)(Math.Pow(ball2center.X - ball1center.X, 2) + Math.Pow(ball2center.Y - ball1center.Y, 2));
+            float massCalculation2 = 2 * ball1.mass / (ball1.mass + ball2.mass);
             Vector2 Oi2 = ball2center - ball1center;
-            ball2.direction -= massCalculation2 * dotProduct2 / (float)centerDistancePow2 * Oi2;
+            float dotProduct2 = Vector2.Dot(ball2.direction - ball1.direction, Oi2);
+
+            ball1.direction -= massCalculation1 * dotProduct1 / centerDistancePow1 * Oi1;
+            ball2.direction -= massCalculation2 * dotProduct2 / centerDistancePow2 * Oi2;
         }
     }
 }
